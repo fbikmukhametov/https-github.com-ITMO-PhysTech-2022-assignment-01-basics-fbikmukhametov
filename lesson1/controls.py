3272a2e87e5564a8dd114284cc0a8ef4d0c06994
Farid <f.bikmukhametov@metalab.ifmo.ru>
2022-10-04 19:15:50 +0300

    Task done

--- lesson1/controls.py
+++ lesson1/controls.py
@@ -1,6 +1,13 @@
 import random
+import string
 x=random.randint(1,10)
 y=random.randint(1,10)
+def generate_random_string(length):
+    letters=string.ascii_lowercase
+    rand_string=''.join(random.choice(letters) for i in range(length))
+    print(rand_string)
+generate_random_string(30)
+
 shagi=0
 print(x,y)
 if x-y>0:
@@ -14,7 +21,5 @@ while x>1:
     else:
         x=3*x+1
         shagi=shagi+1
-
-print('изменени')
 print('количесвто шагов =',shagi)
 
